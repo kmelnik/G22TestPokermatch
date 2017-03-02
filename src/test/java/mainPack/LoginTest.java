3c99a389a5ad3dbe92a1ecc50acf3c7a33e2a275
kmelnik <melnik_ks@ukr.net>
2017-03-02 16:50:05 +0200

    LoginTest and main properties

---
+++ src/test/java/mainPack/LoginTest.java
@@ -0,0 +1,24 @@
+package mainPack;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import parentTest.ParentTest;
+
+/**
+ * Created by k.melnyk on 01.03.2017.
+ */
+public class LoginTest extends ParentTest {
+    public LoginTest(String browser) {
+        super(browser);
+    }
+    @Test
+    public void validLogOn (){
+        driver.get("https://pokermatch.com/");
+        driver.findElement(By.xpath(".//a[@href='#login']")).click(); // find button Login
+        driver.findElement(By.xpath(".//*[@id='login']//*//h4[text()='Authorization']")).isDisplayed();
+        driver.findElement(By.xpath(".//*[@id='login']//input[@name='login']")).sendKeys("mobtest");
+        driver.findElement(By.xpath(".//*[@id='login']//input[@name='password' and @type='text']")).sendKeys("lsd666");
+        driver.findElement(By.xpath(".//*[@id='login']//*//button[@type='button']")).click();
+        checkAC("Element 'Ваш логин: ' found", driver.findElement(By.xpath(".//span[@class='info-list__item-label']")).isDisplayed(), true);
+    }
+}
