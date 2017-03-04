0f6c68b1bf59673417a9c3f362d10f02a2778da3
kmelnik <melnik_ks@ukr.net>
2017-03-04 20:20:11 +0200

    comment find text Authorization

--- src/test/java/mainPack/LoginTest.java
+++ src/test/java/mainPack/LoginTest.java
@@ -15,7 +15,7 @@ public class LoginTest extends ParentTest {
     public void validLogOn (){
         driver.get("https://pokermatch.com/");
         driver.findElement(By.xpath(".//a[@href='#login']")).click(); // find button Login
-        driver.findElement(By.xpath(".//*[@id='login']//*//h4[text()='Authorization']")).isDisplayed();
+        //driver.findElement(By.xpath(".//*[@id='login']//*//h4[text()='Authorization']")).isDisplayed();
         driver.findElement(By.xpath(".//*[@id='login']//input[@name='login']")).sendKeys("mobtest");
         driver.findElement(By.xpath(".//*[@id='login']//input[@name='password' and @type='text']")).sendKeys("lsd666");
         driver.findElement(By.xpath(".//*[@id='login']//*//button[@type='button']")).click();
