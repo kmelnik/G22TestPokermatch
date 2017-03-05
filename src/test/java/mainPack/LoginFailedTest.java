9e046719b5d3c973c58afe1eecb7309e39092f17
kmelnik <melnik_ks@ukr.net>
2017-03-05 17:55:30 +0200

    2 tests - LoginPassed / LoginFailed, deleted - App test

--- src/test/java/mainPack/LoginFailedTest.java
+++ src/test/java/mainPack/LoginFailedTest.java
@@ -1,27 +1,28 @@
 package mainPack;
 
+
 import org.junit.Test;
 import org.openqa.selenium.By;
 import parentTest.ParentTest;
 
+
 /**
- * Created by k.melnyk on 01.03.2017.
+ * Created by k.melnyk on 05.03.2017.
  */
-public class LoginTest extends ParentTest {
-    public LoginTest(String browser) {
+public class LoginFailedTest extends ParentTest {
+    public LoginFailedTest(String browser) {
         super(browser);
     }
-
     @Test
-    public void validLogOn() {
+    public void loginFailedTest() {
         driver.get("https://pokermatch.com/");
         driver.findElement(By.xpath(".//a[@href='#login']")).click(); // find button Login
         driver.findElement(By.name("login")).clear();
-        driver.findElement(By.name("login")).sendKeys("mobtest");
+        driver.findElement(By.name("login")).sendKeys("dsds"); // unvalid login
         driver.findElement(By.xpath(".//*[@id='login']//input[@name='password' and @type='text']")).clear();
         driver.findElement(By.xpath(".//*[@id='login']//input[@name='password' and @type='text']")).sendKeys("lsd666");
         driver.findElement(By.xpath(".//*[@id='login']//form/div[4]/button")).click();
-        checkAC("Failed Expected Result", driver.findElement(By.xpath(".//*[@class='info-list__item info-list__item_type_nickpin']")).isDisplayed(), true);
+        checkAC("Failed Expected Result", driver.findElement(By.xpath(".//*[@id='login']//*[@class='modala__error' and @style='display: block;']")).isDisplayed(), true);
     }
 
 }
